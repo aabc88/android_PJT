80dcd60f4e5990586603820276a4ad99364ad64d
aabc88 <aabc88@nate.com>
2021-02-15 01:03:09 +0900

    ListView Checked-ing

--- IntentTest/app/src/main/java/com/example/intenttest/CustomAdapter.kt
+++ IntentTest/app/src/main/java/com/example/intenttest/CustomAdapter.kt
@@ -6,28 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 
-class Data(val profile:Int, val name: String, val song : String)
+class Data(val profile: Int, val name: String, val song: String, var checked_box: Boolean)
 
-class CustomAdapter (val context: Context, val DataList: ArrayList<Data>)
+class CustomAdapter(val context: Context, val DataList: ArrayList<Data>)
     : BaseAdapter() {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var flag = null
-        val view:View = LayoutInflater.from(context).inflate(R.layout.custom_listview, null)
+        val view: View = LayoutInflater.from(context).inflate(R.layout.custom_listview, null)
         val profile = view.findViewById<ImageView>(R.id.imageView)
         val name = view.findViewById<TextView>(R.id.txt_name)
         val songname = view.findViewById<TextView>(R.id.txt_songname)
         val data = DataList[position]
+        val checked = view.findViewById<CheckBox>(R.id.ch_box)
 
         profile.setImageResource(data.profile)
         view.setBackgroundColor(Color.DKGRAY)
         name.text = data.name
         songname.text = data.song
 
+
+
         return view
     }
+
     override fun getItem(position: Int) = DataList[position]
     override fun getItemId(position: Int) = 0L
     override fun getCount() = DataList.size
--- IntentTest/app/src/main/java/com/example/intenttest/MainActivity.kt
+++ IntentTest/app/src/main/java/com/example/intenttest/MainActivity.kt
@@ -1,28 +1,26 @@
 package com.example.intenttest
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
 import com.example.intenttest.databinding.ActivityMainBinding
 
 var DataList = arrayListOf(
-        Data(R.drawable.ic_launcher_background, "0", "a"),
-        Data(R.drawable.ic_launcher_background, "1", "b"),
-        Data(R.drawable.ic_launcher_background, "2", "c"),
-        Data(R.drawable.ic_launcher_background, "3", "d"),
-        Data(R.drawable.ic_launcher_background, "4", "e"),
-        Data(R.drawable.ic_launcher_background, "5", "f"),
-        Data(R.drawable.ic_launcher_background, "6", "f"),
-        Data(R.drawable.ic_launcher_background, "7", "f"),
-        Data(R.drawable.ic_launcher_background, "8", "f"),
-        Data(R.drawable.ic_launcher_background, "9", "f"),
-        Data(R.drawable.ic_launcher_background, "10", "f"),
-        Data(R.drawable.ic_launcher_background, "11", "f"),
-        Data(R.drawable.ic_launcher_background, "12", "f"),
-        Data(R.drawable.ic_launcher_background, "13", "f")
+        Data(R.drawable.ic_launcher_background, "0", "a", false),
+        Data(R.drawable.ic_launcher_background, "1", "b", false),
+        Data(R.drawable.ic_launcher_background, "2", "c", false),
+        Data(R.drawable.ic_launcher_background, "3", "d", false),
+        Data(R.drawable.ic_launcher_background, "4", "e", false),
+        Data(R.drawable.ic_launcher_background, "5", "f", false),
+        Data(R.drawable.ic_launcher_background, "6", "f", false),
+        Data(R.drawable.ic_launcher_background, "7", "f", false),
+        Data(R.drawable.ic_launcher_background, "8", "f", false),
+        Data(R.drawable.ic_launcher_background, "9", "f", false),
+        Data(R.drawable.ic_launcher_background, "10", "f", false),
+        Data(R.drawable.ic_launcher_background, "11", "f", false),
+        Data(R.drawable.ic_launcher_background, "12", "f", false),
+        Data(R.drawable.ic_launcher_background, "13", "f", false)
 )
 
 class MainActivity : AppCompatActivity() {
@@ -35,8 +33,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
         binding.listView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
         binding.listView.adapter = CustomAdapter(this, DataList)
-
-
     }
 
 
